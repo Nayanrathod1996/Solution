bb57c797a684c83a9ca58dcde8467d5d86c5aefd
Nayan Rathod <nayanrathod515@gmail.com>
2023-09-15 20:29:48 +0530

    Problem 5:Swap Two Numbers without using a temporary variabble Solution in file name Swapvalue.java

--- Swapvalue.java
+++ Swapvalue.java
@@ -1,8 +1,8 @@
 public class Swapvalue {
     public static void main(String[] args) {
-    //     Problem 5: Swap two numbers without using a temporary variable
-    //     Input:integer a = "10", b = "50".
-    //    Output: Strings after swap: a = 50 and b = 10
+    /*     Problem 5: Swap two numbers without using a temporary variable
+         Input:integer a = "10", b = "50".
+       Output: Strings after swap: a = 50 and b = 10 */
     
         int a=10;
         int b=50;
